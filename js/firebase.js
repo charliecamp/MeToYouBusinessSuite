369f48d829aad2550f7e4a6c57c664e9e4059806
charliecamp <charliecampbell1108@icloud.com>
2026-08-06 23:10:10 +0100

    Add Firebase Storage and update exports

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,41 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+
 import {
-  getFirestore
+    getFirestore
 } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCO6e9VWwPV17lt7FKK052hlEdy-lmsOqk",
-  authDomain: "metoyoubusinesssuite.firebaseapp.com",
-  projectId: "metoyoubusinesssuite",
-  storageBucket: "metoyoubusinesssuite.firebasestorage.app",
-  messagingSenderId: "591235577638",
-  appId: "1:591235577638:web:1dec8dcfc23b2573f533e7"
+
+    apiKey: "AIzaSyCO6e9VWwPV17lt7FKK052hlEdy-lmsOqk",
+
+    authDomain: "metoyoubusinesssuite.firebaseapp.com",
+
+    projectId: "metoyoubusinesssuite",
+
+    storageBucket: "metoyoubusinesssuite.firebasestorage.app",
+
+    messagingSenderId: "591235577638",
+
+    appId: "1:591235577638:web:1dec8dcfc23b2573f533e7"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-export { db };
+const storage = getStorage(app);
+
+export {
+
+    app,
+
+    db,
+
+    storage
+
+};
